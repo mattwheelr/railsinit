2b5428ac6becce6394ac2962451f1edfa3456c3f
Matt Wheeler <mattwheeler98@gmail.com>
2013-01-28 21:45:23 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fifilafleur::Application.config.secret_token = '93254cd025b2e4b5a100d13121482c76f5a9078e009398b6e9e2763a34b37472e0267787789c248a02bc9482cb4251adba33863274b8cf57ae0ceaaa88c24c76'
---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+Fifilafleur::Application.routes.draw do
+
+  root :to => 'static_pages#home'
+
+end
